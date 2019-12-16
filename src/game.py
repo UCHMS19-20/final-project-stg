26970f70eadcbc94db73e1f986daa881f7f84680
21smendoza <21smendoza@ucvts.org>
2019-12-16 13:11:01 -0500

    Set up a little

--- src/game.py
+++ src/game.py
@@ -1,7 +1,13 @@
-import time
+import sys
+import pygame
 
-print("Welcome to the sample repository!")
-time.sleep(1)
-print("All the code for your game should go in the src folder")
-time.sleep(1)
-print("Now delete this file and make your own!")
+pygame.init()
+
+screen = pygame.display.set_mode( (550, 700) )
+
+while True:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            sys.exit()
+        

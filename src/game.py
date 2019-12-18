1b64af4405e4cef6fadddfaa5335745a3dea5dbf
21smendoza <21smendoza@ucvts.org>
2019-12-18 12:31:53 -0500

    Added in movement

--- src/game.py
+++ src/game.py
@@ -3,11 +3,35 @@ import pygame
 
 pygame.init()
 
-screen = pygame.display.set_mode( (550, 700) )
+white = (255, 255, 255)
+black = (0, 0, 0)
+screen = pygame.display.set_mode( (650, 600) )
+character_speed = 5
+x = 325
+y = 500
 
 while True:
+    pygame.time.delay(100)
+
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             sys.exit()
-        
+
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            x -= character_speed
+
+        if keys[pygame.K_RIGHT]:
+            x += character_speed
+
+        if keys[pygame.K_UP]:
+            y -= character_speed
+
+        if keys[pygame.K_DOWN]:
+            y += character_speed
+
+        screen.fill(black)
+        pygame.draw.circle(screen, white, (x, y), 10)  
+        pygame.display.update()

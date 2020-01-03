b75570aa86cc64fdf3ac4aaed8e943883c1f92e1
21smendoza <smendoza@ucvts.org>
2020-01-02 20:10:59 -0500

    Changed player to a Player class

--- src/game.py
+++ src/game.py
@@ -3,7 +3,8 @@ import pygame
 
 #initializes the program
 pygame.init()
-
+pygame.mixer.init()
+clock = pygame.time.Clock()
 #if statement has no purpose other than being able to store large amounts of variables
 if True:
     width = 756
@@ -14,74 +15,97 @@ if True:
     pink = (186, 9, 115)
     purple = (83, 11, 122)
     screen = pygame.display.set_mode( (width, height) )
-    character_speed = 10
-    x = 231
-    y = 500
     power = 1
     score = 0
     font_score = pygame.font.SysFont("comicsansms", 20)
     score_text = font_score.render(f"Score: {score} ", True, white)
     font_power = pygame.font.SysFont("comicsansms", 20)
     power_text = font_score.render(f"Power: {power} ", True, white)
-
-
-
-
-
 pygame.display.flip()
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 10))
+        self.image.fill(black)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 261
+        self.rect.bottom = height - 25
+        self.speedx = 0
+        self.speedy = 0
+        self.speed = 5
+
+    def update(self):
+        self.speedx = 0
+        self.speedy = 0
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            if keys[pygame.K_RIGHT]:
+                if keys[pygame.K_LSHIFT]:
+                    self.speedx = -self.speed
+                else:
+                    self.speedx = -self.speed / 2 
+            else:
+                if keys[pygame.K_LSHIFT]:
+                    self.speedx = -self.speed / 2
+                else:
+                    self.speedx = -self.speed
+                    
+        if keys[pygame.K_RIGHT]:
+            if keys[pygame.K_LSHIFT]:
+                self.speedx = self.speed / 2
+            else:
+                self.speedx = self.speed        
+        if keys[pygame.K_UP]:
+            if keys[pygame.K_LSHIFT]:
+                self.speedy = -self.speed / 2
+            else:
+                self.speedy = -self.speed
+                
+        if keys[pygame.K_DOWN]:
+            if keys[pygame.K_LSHIFT]:
+                self.speedy = self.speed / 2
+            else:
+                self.speedy = self.speed
+        if self.rect.right > width / 1.55:
+            self.rect.right = width / 1.55
+        if self.rect.left < 30:
+            self.rect.left = 30
+        if self.rect.top < 15:
+            self.rect.top = 15
+        if self.rect.bottom > height - 15:
+            self.rect.bottom = height - 15
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+    
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+
 #game starts the main loop
 while True:
     #game delays the reading of the code so its not too fast when it runs
-    pygame.time.delay(30)
+    clock.tick(60)
 
     for event in pygame.event.get():
         #exits game when pygame.quit is called
         if event.type == pygame.QUIT:
             sys.exit()
 
-
-    # if statement contains the code for player movement and border collision
-    if True:
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_LEFT]:
-            if keys[pygame.K_RIGHT]:
-                if x > 36:
-                    if keys[pygame.K_LSHIFT]:
-                        x -= 2 * round(character_speed / 2)
-                    else:
-                        x -= 2 * character_speed
-            else:
-                if x > 36:
-                    if keys[pygame.K_LSHIFT]:
-                        x -= round(character_speed / 2)
-                    else:
-                        x -= character_speed
-        if keys[pygame.K_RIGHT]:
-            if x < width / 1.6 + 10:
-                    if keys[pygame.K_LSHIFT]:
-                        x += round(character_speed / 2)
-                    else:
-                        x += character_speed
-        if keys[pygame.K_UP]:
-            if y > 20:
-                if keys[pygame.K_LSHIFT]:
-                    y -= round(character_speed / 2)
-                else:
-                    y -= character_speed
-        if keys[pygame.K_DOWN]:
-            if y < height - 20:
-                if keys[pygame.K_LSHIFT]:
-                    y += round(character_speed / 2)
-                else:
-                    y += character_speed
-
+    all_sprites.update()
     screen.fill(pink)
-    pygame.draw.circle(screen, white, (x, y), 5)
-    pygame.draw.rect(screen, purple, (round(width / 1.6) + 20, 0, width - width / 2, height))
+    all_sprites.draw(screen)
+    pygame.draw.rect(screen, purple, (round(width / 1.55), 0, width - width / 2, height))
     pygame.draw.rect(screen, purple, (0, 0, width, 15))
     pygame.draw.rect(screen, purple, (0, height - 15, width, 20))
     pygame.draw.rect(screen, purple, (0, 0, 30, height))
-    screen.blit(score_text, (500, 10) )
-    screen.blit(power_text, (500, 80) )
+    screen.blit(score_text, (width / 1.5, 10) )
+    screen.blit(power_text, (width / 1.5, 80) )
     pygame.display.update()
+
+"""Things left to do for next:
+- Replace circle player with a player class
+- Make bullet class
+- Make enemy class"""
